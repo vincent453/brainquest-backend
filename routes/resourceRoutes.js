54bfe5ba32cc9fd978b4fe7dd18a9430d3a8f4ab
Lawrence Vincent <lawrencevincent453@gmail.com>
2026-01-20 18:58:49 -0800

    update

--- routes/resourceRoutes.js
+++ routes/resourceRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const resourceController = require('../controllers/ResourceController');
+const resourceController = require('../controllers/resourceController');
 const { authenticate, requireAdmin } = require('../middleware/auth');
 const upload = require('../middleware/upload');
 
