463d45ded05f7e9b7708cb28393903e5b9882fd7
Lawrence Vincent <lawrencevincent453@gmail.com>
2026-08-05 02:52:21 +0100

    -u

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ if (!fs.existsSync(uploadsDir)) {
 const isDevelopment = process.env.NODE_ENV === "development";
 
 const allowedOrigins = [
-  "http://localhost:3000",
+  "https://brainquest-app.vercel.app/",
   process.env.FRONTEND_URL,
 ].filter(Boolean);
 
